21a40690a585dc5382a44388e9f52add579d0731
Cristiane Fournier <almeidafournier@gmail.com>
2025-05-25 23:48:31 +0000

    Implementado criação jogo Xadrez

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,36 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+    int numero = 0, i=0;
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    while (i<=5)
+    {
+        if (i !=0)
+        {
+            printf("Bispo na diagonal (para cima e a direita) \n");//imprime a direção do movimento
+        }
+        i++;
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    //mover a Torre 5 casas para direita
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Torre a Direita\n");//imprime a direção do movimento
+    }
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    do
+    {
+        printf("Rainha andando para esquerda\n");
+        numero ++;
+    }        
+    while (numero < 8);
+    
+    printf("\n####   Fim do jogo!   ####\n\n");
+
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
